fbd561efa8afc9974b1151d0f04c3f32580e3fe8
ubuntu <aitechmanml@gmail.com>
2025-03-31 11:42:13 +0530

    hello

---
+++ EDAPro/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="edapro",
+    version="0.1.0",
+    description="A Python library for easy exploratory data analysis",
+    author="aitechman",
+    author_email="aitechman@gmail.com",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "seaborn",
+        "matplotlib",
+        "scipy",
+        "statsmodels",
+        "scikit-learn",
+        "wordcloud",
+        "ipython"
+    ],
+    python_requires=">=3.8",
+)
