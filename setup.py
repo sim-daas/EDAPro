e3be7849cc9260b7db4143528799d7b5bda76660
ubuntu <aitechmanml@gmail.com>
2025-03-31 12:06:52 +0530

    hello

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="edapro",
     version="0.1.0",
-    license_file='LICENSE',
     description="A Python library for easy exploratory data analysis",
     author="aitechman",
     author_email="aitechman@gmail.com",
